290559967fcbd3d8bd033260795dbc112103cdd8
IanHMV <ianvazquez418@gmail.com>
2022-09-24 15:31:14 -0500

    "Ejercicio de mostrar suma y validar division COMPLETADO"

--- datos.php
+++ datos.php
@@ -12,9 +12,20 @@ numero1 y numero2 es el name que le pusiste al input ej.
 
 $numero1 = $_POST['numero1'];
 $numero2 = $_POST['numero2'];
-echo "mi primer numero es: ". $_POST['numero1'] .'<br>';
-echo "mi segundo número es: $numero2";
+$resultado1 = $numero1+$numero2;
+#echo "mi primer numero es: ". $_POST['numero1'] .'<br>';
+#echo "mi segundo número es: $numero2";
 
+echo "mi primer numero es: ". $_POST['numero1'] .'<br>';
+echo "mi segundo número es: $numero2" .'<br>';
+echo "el resultado de la suma es: $resultado1" .'<br>';
+
+if($numero2==0) {
+    echo "El segundo numero no puede ser 0 para ejecutar la division" ;
+}else{
+    $resultado2 = $numero1/$numero2;
+    echo "el resultado de la division es: $resultado2";
+}
 
 //Ejercicio 1 haz la suma de los dos numeros aquí ... e imprimelos
 
